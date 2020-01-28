6c6567758bb4a6d7ea42f97b5d55116ba014c864
nyola mike <iamnyola@gmail.com>
2020-01-27 21:45:20 -0800

    adds test case: Stores users reactions by content_id

--- reactions_server/test/reactions_server_test.exs
+++ reactions_server/test/reactions_server_test.exs
@@ -1,8 +1,19 @@
 defmodule ReactionsServerTest do
-  use ExUnit.Case, 
+  use ExUnit.Case, async: true
   doctest ReactionsServer
 
-  test "greets the world" do
-    assert ReactionsServer.hello() == :world
+  test "Stores users reactions by content_id " do
+    request_pay_load = %{
+      :type => "reaction",
+      :action => "add",
+      :content_id => "056af828-2efe-4631-8446-c52cabb67367",
+      :user_id => "9e204fff-9b48-4000-8b21-6cc88be2f01e",
+      :reaction_type => "fire"
+    }
+    {:ok, agent_process} = ReactionsServer.ReactionsAgent.start_link(%{})
+    assert ReactionsServer.ReactionsAgent.get(agent_process, request_pay_load.content_id) == nil
+
+    ReactionsServer.ReactionsAgent.put(agent_process, request_pay_load.content_id, request_pay_load)
+    assert ReactionsServer.ReactionsAgent.get(agent_process, request_pay_load.content_id) == Map.put(%{},request_pay_load.user_id,request_pay_load)
   end
 end
