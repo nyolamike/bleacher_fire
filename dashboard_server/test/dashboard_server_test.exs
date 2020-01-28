5b46031fe6fc262d6b8cb26afe13d40a5b4e0d09
nyola mike <iamnyola@gmail.com>
2020-01-28 01:58:05 -0800

    adds test to increament content reactions count

--- dashboard_server/test/dashboard_server_test.exs
+++ dashboard_server/test/dashboard_server_test.exs
@@ -1,8 +1,13 @@
 defmodule DashboardServerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest DashboardServer
 
-  test "greets the world" do
-    assert DashboardServer.hello() == :world
+  test "Increments count for a particular content_id " do
+    content_id =  "056af828-2efe-4631-8446-c52cabb67367"
+    {:ok, agent_process} = ReactionsServer.DashboardAgent.start_link(%{})
+    assert ReactionsServer.DashboardAgent.get(agent_process, content_id) == 0
+
+    ReactionsServer.DashboardAgent.increament(agent_process, content_id)
+    assert ReactionsServer.DashboardAgent.get(agent_process, content_id) == 1
   end
 end
