685c03fa1790c325aa257a4b561a76849307ebf9
nyola mike <iamnyola@gmail.com>
2020-01-28 03:33:10 -0800

    adds test: Stores users content_ids of reactions

--- users_server/test/users_server_test.exs
+++ users_server/test/users_server_test.exs
@@ -1,8 +1,18 @@
 defmodule UsersServerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest UsersServer
 
-  test "greets the world" do
-    assert UsersServer.hello() == :world
+  test "Stores users content_ids of reactions " do
+    request_pay_load = %{
+      content_id: "056af828-2efe-4631-8446-c52cabb67367",
+      user_id: "9e204fff-9b48-4000-8b21-6cc88be2f01e"
+    }
+    {:ok, agent_process} = ReactionsServer.UsersAgent.start_link(%{})
+    assert ReactionsServer.UsersAgent.get(agent_process, request_pay_load.user_id) == nil
+
+    ReactionsServer.UsersAgent.put(agent_process, request_pay_load.content_id, request_pay_load)
+    assert ReactionsServer.UsersAgent.get(agent_process, request_pay_load.content_id) == %{ request_pay_load.user_id => [request_pay_load.content_id] }
   end
+
+
 end
