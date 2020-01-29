70a546d95e50c72b8a0ec068f5824a9112893731
nyola mike <iamnyola@gmail.com>
2020-01-29 10:10:40 -0800

    tests POST 404 /api/reaction remove

--- bleacher_fire/test/bleacher_fire_web/controllers/reaction_controller_test.exs
+++ bleacher_fire/test/bleacher_fire_web/controllers/reaction_controller_test.exs
@@ -14,4 +14,20 @@ defmodule BleacherFireWeb.ReactionControllerTest do
     response = json_response(conn, 200)
     assert Map.has_key?(response, user_id) and response[user_id]["user_id"]  == user_id
   end
+
+  test "POST 404 /api/reaction remove  ", %{conn: conn} do
+    content_id = "7056af828-hh2efe-4631-xd8446-75tdxxx-8118"
+    user_id = "some_user@bleacher_fire.com"
+    conn = post(conn, "/api/reaction", %{
+        type: "reaction", 
+        action: "remove", 
+        content_id: content_id,
+        user_id:  user_id,
+        reaction_type: "fire"
+    })
+    html_body = html_response(conn, 404)
+    assert html_body == ""
+  end
+
+
 end
