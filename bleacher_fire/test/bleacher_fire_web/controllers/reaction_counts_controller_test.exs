5728de7cac78688841494a51a478b02ddfab78e2
nyola mike <iamnyola@gmail.com>
2020-01-29 09:59:09 -0800

    tests /reaction_counts/ with exisiting content_id value

--- bleacher_fire/test/bleacher_fire_web/controllers/reaction_counts_controller_test.exs
+++ bleacher_fire/test/bleacher_fire_web/controllers/reaction_counts_controller_test.exs
@@ -7,16 +7,20 @@ defmodule BleacherFireWeb.ReactionCountsControllerTest do
     assert html_body == ""
   end
 
-  # test "GET /api/reaction_counts/7056af828-2efe-4631-8446-75tdxxx-777", %{conn: conn} do
-  #   #first add in a post
-
-  #   conn = get(conn, "/api/reaction_counts/7056af828-2efe-4631-8446-75tdxxx-777")
-  #   html_body = html_response(conn, 404)
-  #   assert html_body == ""
-
-    
-  #   # IO.inspect(body)
-  #   # assert Map.has_key?(body, "reaction_count") and body["reaction_count"] == %{"fire" => 0}
-  # end
+  test "GET /api/reaction_counts/7056af828-2efe-4631-8446-75tdxxx-687", %{conn: conn} do
+    #first add in a post
+    content_id = "7056af828-2efe-4631-8446-75tdxxx-687"
+    user_id = "some_user@bleacher_fire.com"
+    conn = post(conn, "/api/reaction", %{
+        type: "reaction", 
+        action: "add", 
+        content_id: content_id,
+        user_id:  user_id,
+        reaction_type: "fire"
+    })
+    conn = get(conn, "/api/reaction_counts/" <> content_id)
+    body = json_response(conn, 200)
+    assert Map.has_key?(body, "reaction_count") and body["reaction_count"] == %{"fire" => 1}
+  end
   
 end
